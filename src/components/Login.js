b3ff001c34a9ddb3923840f54efdb42e7878f62d
Jorge Gonzalez <jglez.cs@gmail.com>
2021-01-05 02:33:03 -0600

    Added bg images, added icons for inputes, and styled <Login /> component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,49 @@
 import React from "react";
 import Navbar from "./Navbar";
+import '../assets/stylesheets/forms.css'
 
 export default function Login(props) {
   const { values, submit, change, disabled, errors } = props;
 
   return (
-    <div className='login-container'>
+    <>
       <Navbar />
-      <h1>Log in</h1>
-      <form className='text-container' onSubmit={submit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={values.email}
-            onChange={change}
-            placeholder='johndoe@email.com'
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={values.password}
-            onChange={change}
-            placeholder='secure password'
-          />
-        </label>
+      <div className='login-container'>
+        <h1>Login</h1>
+        <form onSubmit={submit}>
+          <div className='input-container'>
+            <i class="fas fa-user"></i>
+            <input
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={change}
+              placeholder='johndoe@email.com'
+            />
+          </div>
 
-        {/* Errors */}
-        <div className="error">{errors.email}</div>
-        <div className="error">{errors.password}</div>
-        <input
-          type="submit"
-          value="Submit"
-          id="submitBtn"
-          disabled={disabled}
-        />
-      </form>
-    </div>
+          <div className='input-container'>
+            <i class="fas fa-lock"></i>
+            <input
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={change}
+              placeholder='secure password'
+            />
+          </div>
+
+          {/* Errors */}
+          <div className="error">{errors.email}</div>
+          <div className="error">{errors.password}</div>
+          <input
+            type="submit"
+            value="Submit"
+            id="submitBtn"
+            disabled={disabled}
+          />
+        </form>
+      </div>
+    </>
   );
 }
