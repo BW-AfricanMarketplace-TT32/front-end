4b15682ad6e899143329a783dc8beda42bbc2c23
c_melchor <cah.melchor@gmail.com>
2021-01-05 09:08:19 -0800

    added reducer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducers";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
