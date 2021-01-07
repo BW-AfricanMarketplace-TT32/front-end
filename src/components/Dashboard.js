8126374b725a17264e81c138ae70bf418f536235
c_melchor <cah.melchor@gmail.com>
2021-01-07 11:26:00 -0800

    code cleanup

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { axiosWithAuth } from "../auth/axiosWithAuth";
 import Navbar from "./Navbar";
 import Items from "./Items";
